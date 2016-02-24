76e3ade140090b437333be1d4364ed28181a95dd
zabrahair <zabrahair@gmail.com>
2016-02-24 17:11:36 +0800

    change production to production.ini in ansible project initilization project

--- ansible/ansible_project_initializer/ansible-project-init.sh
+++ ansible/ansible_project_initializer/ansible-project-init.sh
@@ -2,4 +2,4 @@
 
 root_path=$1
 project_name=$2
-ansible-playbook -i production -c local --extra-vars "root_path=${root_path} project_name=${project_name}" -t init main.yml 
+ansible-playbook -i production.ini -c local --extra-vars "root_path=${root_path} project_name=${project_name}" -t init main.yml
